7a05c4f70d10e733ef0ff4a0cfddb71cdd649258
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-02 17:16:33 +0100

    second js event done

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const features = document.querySelector(".features");
 const droplist = document.getElementById("toggleFeautures");
+const company = document.querySelector(".company");
+const droplist2 = document.getElementById("toggleCompany");
 
 features.addEventListener("click", () => {
   droplist.classList.toggle("hidden");
+});
+
+company.addEventListener("click", () => {
+  droplist2.classList.toggle("hiddenCompany");
 });
